47f50d7d68c9da419cd60b5a14ba860bafdffb33
priyankachalla <challa.priyanka@gmail.com>
2026-07-13 07:06:14 -0500

    lab03

---
+++ EX_1/Lab03.py
@@ -0,0 +1,23 @@
+price = 19.99
+quantity = 3
+
+total = price * quantity
+print(round(total, 2))
+
+print("checking again to see")
+
+
+# Python script to calculate square and cube
+
+# Take input from the user
+num = float(input("Enter a number: "))
+
+# Calculate square
+square = num ** 2
+
+# Calculate cube
+cube = num ** 3
+
+# Display the results
+print(f"The square of {num} is {square}")
+print(f"The cube of {num} is {cube}"
